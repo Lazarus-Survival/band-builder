3ddcd9a4c415e0776e455c0f2dac44474199eb95
javierc1989-spec <javierc1989@gmail.com>
2026-09-14 12:20:56 +0200

    Load visual character card assets

--- protection-layout.js
+++ protection-layout.js
@@ -124,3 +124,13 @@ sheetMember = function(member,index) {
 };
 
 render();
+
+(function loadVisualCards(){
+  const css=document.createElement("link");
+  css.rel="stylesheet";
+  css.href="card-visuals.css?v=20260914-1";
+  document.head.appendChild(css);
+  const script=document.createElement("script");
+  script.src="card-visuals.js?v=20260914-1";
+  document.body.appendChild(script);
+})();
