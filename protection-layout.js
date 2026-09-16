1ec7b8cdcdfaebdd9f12fdcb84c688d88a7d83ee
Javier <javierc1989@gmail.com>
2026-09-16 15:07:21 +0200

    Integrate approved high-resolution weapon artwork

--- protection-layout.js
+++ protection-layout.js
@@ -125,9 +125,9 @@ render();
 (function loadVisualCards(){
   const css=document.createElement("link");
   css.rel="stylesheet";
-  css.href="card-visuals.css?v=20260916-8";
+  css.href="card-visuals.css?v=20260916-10";
   document.head.appendChild(css);
   const script=document.createElement("script");
-  script.src="card-visuals.js?v=20260916-9";
+  script.src="card-visuals.js?v=20260916-10";
   document.body.appendChild(script);
 })();
