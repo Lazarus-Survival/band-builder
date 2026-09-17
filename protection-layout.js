36a094dcd3f6068f53479beaaed6f2e547fc6956
Javier <javierc1989@gmail.com>
2026-09-17 19:18:58 +0200

    Keep entire printed page background white

--- protection-layout.js
+++ protection-layout.js
@@ -125,9 +125,9 @@ render();
 (function loadVisualCards(){
   const css=document.createElement("link");
   css.rel="stylesheet";
-  css.href="card-visuals.css?v=20260917-2";
+  css.href="card-visuals.css?v=20260917-3";
   document.head.appendChild(css);
   const script=document.createElement("script");
-  script.src="card-visuals.js?v=20260917-2";
+  script.src="card-visuals.js?v=20260917-3";
   document.body.appendChild(script);
 })();
